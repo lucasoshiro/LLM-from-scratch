96eda14e7c39c9ba0c7df0666fc453c3de887112
Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>
2026-04-21 13:35:34 -0300

    feat: bpe-based tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -3,76 +3,46 @@
 from typing import TextIO
 from collections import OrderedDict
 from sys import argv, stderr
-import re
-
-def split(s: str) -> list[str]:
-    SPLIT_REGEX = r'[,.?!();:"\']|\s|--'
-    return [
-        token
-        for token in re.split(SPLIT_REGEX, s)
-        if token
-    ]
+from bpe import Encoder
+from book_parser import iterate_paragraph
 
 class Vocabulary:
+    encoder: Encoder
     path: str
-    vocab: dict[str, int]
-    vocab_list: list[str]
 
     def __init__(self, path):
         self.path = path
-        self.vocab = {}
-        self.vocab_list = []
-
+        
         try:
             self.deserialize()
         except FileNotFoundError:
-            self.extend(['<|endoftext|>'])
+            self.encoder = Encoder(pct_bpe=0.88, lowercase=False)
 
     def deserialize(self) -> None:
-        with open(self.path) as f:
-            self.extend([
-                line.strip()
-                for line in f
-            ])
+        self.encoder = Encoder.load(self.path)
 
-    def extend(self, tokens: list[str]) -> None:
-        vocab_size = len(self.vocab)
-        new_tokens = {
-            token: vocab_size + i
-            for i, token in enumerate({*tokens} - {*self.vocab})
-        }
-        self.vocab |= new_tokens
-        self.vocab_list = [*self.vocab]
+    def extend(self, texts: list[str]) -> None:
+        self.encoder.fit(texts)
 
     def serialize(self) -> None:
-        with open(self.path, 'w') as f:
-            for token in self.vocab:
-                print(token, file=f)
+        self.encoder.save(self.path)
 
     def __str__(self) -> str:
-        return str(self.vocab)
+        return str(self.encoder)
 
     def encode(self, s: str) -> list[int | None]:
-        splitted = split(s)
-        return [
-            self.vocab.get(s)
-            for s in splitted
-        ]
+        return next(self.encoder.transform([s]))
 
     def decode(self, tokens: list[int | None]) -> str:
-        return ' '.join([
-            self.vocab_list[t] if t is not None else 'None'
-            for t in tokens
-        ])
+        return next(self.encoder.inverse_transform([tokens]))
 
 def populate(voc: Vocabulary, path: str) -> None:
     with open(path) as f:
-        for line in f:
-            splitted = split(line.strip())
-            voc.extend(splitted)
+        paragraphs = iterate_paragraph(f)
+        voc.extend([*paragraphs])
 
 if __name__ == '__main__':
-    voc = Vocabulary('voc.txt')
+    voc = Vocabulary('voc.json')
     _, *args = argv
 
     for arg in args:
@@ -84,8 +54,6 @@ if __name__ == '__main__':
             s = input()
         except:
             break
-
-        splitted = split(s)
 
         encoded = voc.encode(s)
         print(encoded)
