e129dd5550d093d397d2c86873cf65a507caa88f
Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>
2026-08-03 21:32:33 -0300

    feat: add causal attention

--- self_attention.py
+++ self_attention.py
@@ -4,20 +4,35 @@ import torch
 import torch.nn as nn
 
 class SelfAttentention(nn.Module):
-    def __init__(self, d_in, d_out, qkv_bias=False):
+    def __init__(self, d_in, d_out, context_length, dropout=0.5, qkv_bias=False):
         super().__init__()
+
+        self.d_out = d_out
 
         self.wq = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.wk = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.wv = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+
+        self.register_buffer(
+            'mask',
+            torch.triu(torch.ones(context_length, context_length), 1)
+        )
+
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
+        b, n, d_in = x.shape
+
         ks = self.wk(x)
         vs = self.wv(x)
         qs = self.wq(x)
 
-        omegas = qs @ ks.T
-        alphas = torch.softmax(omegas / ks.shape[-1] ** 0.5, dim=-1)
+        omegas = qs @ ks.transpose(1, 2)
+        # mask = torch.triu(torch.ones(omegas.shape), 1).bool()
+        masked = omegas.masked_fill(self.mask.bool()[:n, :n], -torch.inf)
+        alphas = torch.softmax(masked / ks.shape[-1] ** 0.5, dim=-1)
+        alphas = self.dropout(alphas)
         return alphas @ vs
 
 if __name__ == '__main__':
@@ -32,6 +47,8 @@ if __name__ == '__main__':
          ]
     )
 
-    sa = SelfAttentention(3, 2)
-    f = sa.forward(inputs)
+    batch = torch.stack((inputs, inputs), dim=0)
+
+    sa = SelfAttentention(3, 2, batch.shape[1], dropout=0.0)
+    f = sa(batch)
     print(f)
